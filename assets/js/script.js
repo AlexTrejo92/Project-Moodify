2008552ac7931ff68164cb4e4c315b03dbc50b27
Isa <iruizord@gmail.com>
2023-04-24 20:02:39 -0600

    adding event listener to dropdowns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
-// mobile navbar menu
-var burgerIcon = document.querySelector('#burger');
-var navbarMenu = document.querySelector('#nav-links');
+// // mobile navbar menu
+// var burgerIcon = document.querySelector('#burger');
+// var navbarMenu = document.querySelector('#nav-links');
 
-burgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('is-active')
+// burgerIcon.addEventListener('click', () => {
+//     navbarMenu.classList.toggle('is-active')
+// })
+
+var menu = document.getElementById("dropdown");
+menu.addEventListener('click', e=>{
+    e.stopPropagation()
+    menu.classList.toggle('is-active')
 })
